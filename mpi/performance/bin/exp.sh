4c3941d5744f949c83bf81a879fb2e07fe5ed8db
supunkamburugamuve <supun06@gmail.com>
2018-01-08 22:12:48 -0500

    adding execution scripts

---
+++ mpi/performance/bin/exp.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+./run.sh 16 2 128000 10
--- mpi/performance/bin/run.sh
+++ mpi/performance/bin/run.sh
@@ -2,4 +2,4 @@
 
 #debug=-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005
 debug=
-mpirun -np 8 --hostfile nodes java $debug -cp ../target/mpi-performance-1.0-SNAPSHOT-jar-with-dependencies.jar edu.iu.dsc.tws.mpiapps.Program --collective $1 --size $2 --itr $3
+mpirun -np $1 --hostfile nodes java $debug -cp ../target/mpi-performance-1.0-SNAPSHOT-jar-with-dependencies.jar edu.iu.dsc.tws.mpiapps.Program --collective $2 --size $3 --itr $4
