ea0251a58ea09740c4f80de433922adcbabc6db8
supunkamburugamuva <supun06@gmail.com>
2018-03-11 23:32:46 -0400

    adding debug code

--- twister2/performance/src/main/java/edu/iu/dsc/tws/apps/stream/IntAllReduceStream.java
+++ twister2/performance/src/main/java/edu/iu/dsc/tws/apps/stream/IntAllReduceStream.java
@@ -34,6 +34,8 @@ public class IntAllReduceStream implements IContainer {
 
   private List<Integer> tasksOfThisExec;
 
+  private Map<Integer, Queue<Integer>> times = new HashMap<>();
+
   @Override
   public void init(Config cfg, int containerId, ResourcePlan plan) {
     LOG.log(Level.INFO, "Starting AllReduceStreaming Example");
@@ -78,8 +80,13 @@ public class IntAllReduceStream implements IContainer {
 
     InternalMessageWorker reduceWorker = null;
     for (int i : tasksOfExecutor) {
-      reduceWorker = new InternalMessageWorker(i, jobParameters, reduce, dataGenerator, DataType.INT_OBJECT);
+      LinkedList<Integer> ackQueue = new LinkedList<>();
+      reduceWorker = new InternalMessageWorker(id, i, jobParameters, reduce, dataGenerator, DataType.INT_OBJECT, ackQueue);
       reduceWorkers.put(i, reduceWorker);
+
+      int sourceIndex = taskOfExecutorList.indexOf(i);
+      times.put(reduceTaskOfExecutorList.get(sourceIndex), ackQueue);
+
       // the map thread where datacols is produced
       Thread mapThread = new Thread(reduceWorker);
       mapThread.start();
@@ -100,38 +107,16 @@ public class IntAllReduceStream implements IContainer {
   public class FinalReduceReceiver implements ReduceReceiver {
     boolean done = false;
 
-    Map<Integer, List<Long>> times = new HashMap<>();
-
     @Override
     public void init(Config cfg, DataFlowOperation op, Map<Integer, List<Integer>> expectedIds) {
       LOG.info(String.format("Initialize: " + expectedIds));
-      for (Map.Entry<Integer, List<Integer>> e : expectedIds.entrySet()) {
-        times.put(e.getKey(), new ArrayList<>());
-      }
     }
 
     @Override
     public boolean receive(int target, Object object) {
-      long time = (System.currentTimeMillis() - startSendingTime);
-      List<Long> timesForTarget = times.get(target);
-      timesForTarget.add(Utils.getTime());
-
-      try {
-        if (timesForTarget.size() >= jobParameters.getIterations()) {
-          List<Long> times = reduceWorkers.get(tasksOfThisExec.get(0)).getStartOfEachMessage();
-
-          long average = 0;
-          for (int i = 0; i < times.size(); i++) {
-            average += (timesForTarget.get(i) - times.get(i));
-          }
-          LOG.info(String.format("%d Finished %d %d", id, target, time));
-          LOG.info(String.format("%d Average: %d", id, average / (times.size())));
-          done = true;
-        }
-      } catch (Throwable r) {
-        LOG.log(Level.SEVERE, String.format("%d excpetion %s %s", id, tasksOfThisExec, reduceWorkers.keySet()), r);
-      }
-
+//      LOG.info(String.format("%d Received message %d keyset  %s", id, target, times.keySet()));
+//      LOG.info(String.format("%d Received message %d size %d", id, target, times.get(target).size()));
+      times.get(target).add(0);
       return true;
     }
 
--- twister2/performance/src/main/java/edu/iu/dsc/tws/apps/stream/InternalMessageWorker.java
+++ twister2/performance/src/main/java/edu/iu/dsc/tws/apps/stream/InternalMessageWorker.java
@@ -1,14 +1,18 @@
 package edu.iu.dsc.tws.apps.stream;
 
 import edu.iu.dsc.tws.apps.data.DataGenerator;
+import edu.iu.dsc.tws.apps.data.DataSave;
 import edu.iu.dsc.tws.apps.data.DataType;
 import edu.iu.dsc.tws.apps.utils.JobParameters;
 import edu.iu.dsc.tws.apps.utils.Utils;
 import edu.iu.dsc.tws.comms.api.DataFlowOperation;
 import edu.iu.dsc.tws.comms.api.MessageFlags;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class InternalMessageWorker implements Runnable {
@@ -24,16 +28,28 @@ public class InternalMessageWorker implements Runnable {
 
   private JobParameters jobParameters;
 
-  private List<Long> startOfMessages;
+  private List<Long> emitTimes;
 
   Object data;
 
-  public InternalMessageWorker(int task, JobParameters jobParameters, DataFlowOperation op, DataGenerator dataGenerator, DataType genString) {
+  private Queue<Integer> acks;
+
+  private int ackCount = 0;
+
+  private List<Long> finalTimes = new ArrayList<>();
+
+  private boolean stop = false;
+
+  private int executor;
+
+  public InternalMessageWorker(int executor, int task, JobParameters jobParameters, DataFlowOperation op, DataGenerator dataGenerator, DataType genString, Queue<Integer> acks) {
     this.task = task;
     this.jobParameters = jobParameters;
     this.operation = op;
     this.generator = dataGenerator;
-    this.startOfMessages = new ArrayList<>();
+    this.emitTimes = new ArrayList<>();
+    this.acks = acks;
+    this.executor = executor;
 
     if (genString == DataType.STRING) {
       data = generator.generateStringData();
@@ -51,6 +67,7 @@ public class InternalMessageWorker implements Runnable {
     startSendingTime = System.currentTimeMillis();
     int iterations = jobParameters.getIterations();
     for (int i = 0; i < iterations; i++) {
+      ack();
       int flag = 0;
       if (i == iterations - 1) {
         flag = MessageFlags.FLAGS_LAST;
@@ -66,7 +83,11 @@ public class InternalMessageWorker implements Runnable {
           }
         }
       }
-      startOfMessages.add(Utils.getTime());
+      emitTimes.add(Utils.getTime());
+    }
+
+    while (true) {
+      ack();
     }
   }
 
@@ -75,6 +96,37 @@ public class InternalMessageWorker implements Runnable {
   }
 
   public List<Long> getStartOfEachMessage() {
-    return startOfMessages;
+    return emitTimes;
+  }
+
+  public void ack() {
+    long time;
+//    LOG.log(Level.INFO, "Acks size ** " + acks.size());
+    Integer ack = acks.poll();
+    while (ack != null) {
+      time = emitTimes.get(ackCount);
+      ackCount++;
+//      LOG.log(Level.INFO, "Ack received index" + ackCount);
+      finalTimes.add(Utils.getTime() - time);
+      long totalTime = System.currentTimeMillis() - startSendingTime;
+      if (ackCount >= jobParameters.getIterations() - 1) {
+        long average = 0;
+        int half = finalTimes.size() / 2;
+        int count = 0;
+        for (int i = half; i < finalTimes.size() - half / 4; i++) {
+          average += finalTimes.get(i);
+          count++;
+        }
+        average = average / count;
+        LOG.info(String.format("%d %d Finished %d total: %d average: %f", executor, task, ackCount, totalTime, average / 1000000.0));
+        try {
+          DataSave.saveList(jobParameters.getFileName() + "" + task + "partition_" + jobParameters.getSize() + "x" + jobParameters.getIterations(), finalTimes);
+        } catch (FileNotFoundException e) {
+          e.printStackTrace();
+        }
+        stop = true;
+      }
+      ack = acks.poll();
+    }
   }
 }
