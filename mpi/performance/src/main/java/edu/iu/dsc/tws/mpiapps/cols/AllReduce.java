bb0c30f42022e845ec9bd671189d708c7fcacae2
supunkamburugamuve <supun06@gmail.com>
2019-06-23 20:03:37 -0400

    adding a simple terasort

--- mpi/performance/src/main/java/edu/iu/dsc/tws/mpiapps/cols/AllReduce.java
+++ mpi/performance/src/main/java/edu/iu/dsc/tws/mpiapps/cols/AllReduce.java
@@ -3,29 +3,28 @@ package edu.iu.dsc.tws.mpiapps.cols;
 import edu.iu.dsc.tws.mpiapps.Collective;
 import mpi.*;
 
-import java.nio.ByteBuffer;
-import java.nio.IntBuffer;
+import java.nio.DoubleBuffer;
 
 public class AllReduce extends Collective {
 
   private long reduceTime = 0;
 
-  private int[] values;
+  private double[] values;
 
   public AllReduce(int size, int iterations) {
     super(size, iterations);
-    values = new int[size];
+    values = new double[size];
   }
 
   @Override
   public void execute() throws MPIException {
     int rank = MPI.COMM_WORLD.getRank();
-    IntBuffer sendBuffer = MPI.newIntBuffer(size);
-    IntBuffer receiveBuffer = MPI.newIntBuffer(size);
+    DoubleBuffer sendBuffer = MPI.newDoubleBuffer(size);
+    DoubleBuffer receiveBuffer = MPI.newDoubleBuffer(size);
     for (int i = 0; i < iterations; i++) {
       sendBuffer.clear();
       sendBuffer.put(values);
-      MPI.COMM_WORLD.allReduce(sendBuffer, receiveBuffer, size, MPI.BYTE, MPI.SUM);
+      MPI.COMM_WORLD.allReduce(sendBuffer, receiveBuffer, size, MPI.DOUBLE, MPI.SUM);
       receiveBuffer.clear();
       sendBuffer.clear();
     }
