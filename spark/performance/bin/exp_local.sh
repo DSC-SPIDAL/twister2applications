6d69ac25a859686bb81e8a0eefc27f95272a9ee1
Vibhatha Abeykoon <vibhatha@gmail.com>
2019-10-11 17:39:24 -0400

    streaming test local scripts;spark

---
+++ spark/performance/bin/exp_local.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+./run_local.sh 100 100 2 0
---
+++ spark/performance/bin/run_local.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#export SPARK_HOME=/N/u/skamburu/deploy/spark2/spark-2.1.0-bin-hadoop2.7
+pathToJar=/home/vibhatha/github/twister2applications/spark/performance/target/spark-performance-1.0-SNAPSHOT-jar-with-dependencies.jar
+masterURL=spark://localhost:7077
+para=4
+ec=1
+em=$(( 1 * $ec ))G
+spark-submit --executor-cores $ec --executor-memory $em --class "edu.iu.dsc.tws.sparkapps.Driver" --master $masterURL $pathToJar -size $1 -iter $2 -col $3 -para $para
