1c3dbce554ef2015807b471cf1a98c43177174dc
Vibhatha Abeykoon <vibhatha@gmail.com>
2019-11-12 08:47:42 -0500

    one-way latency code added

--- flink/performance/src/main/java/edu/iu/dsc/tws/flinkapps/stream/StreamingGather.java
+++ flink/performance/src/main/java/edu/iu/dsc/tws/flinkapps/stream/StreamingGather.java
@@ -1,6 +1,7 @@
 package edu.iu.dsc.tws.flinkapps.stream;
 
 import edu.iu.dsc.tws.flinkapps.data.CollectiveData;
+import edu.iu.dsc.tws.flinkapps.util.GetInfo;
 import org.apache.flink.api.common.functions.*;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.ParameterTool;
@@ -14,112 +15,131 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StreamingGather {
-  int size;
-  int iterations;
-  StreamExecutionEnvironment env;
-  String outFile;
+    int size;
+    int iterations;
+    StreamExecutionEnvironment env;
+    String outFile;
 
-  public StreamingGather(int size, int iterations, StreamExecutionEnvironment env, String outFile) {
-    this.size = size;
-    this.iterations = iterations;
-    this.env = env;
-    this.outFile = outFile;
-  }
-
-  public void execute() {
-    DataStream<CollectiveData> stringStream = env.addSource(new RichParallelSourceFunction<CollectiveData>() {
-      int i = 1;
-      int count = 0;
-      int size = 0;
-      int iterations = 10000;
-
-      @Override
-      public void open(Configuration parameters) throws Exception {
-        super.open(parameters);
-        ParameterTool p = (ParameterTool)
-            getRuntimeContext().getExecutionConfig().getGlobalJobParameters();
-        size = p.getInt("size", 128000);
-        iterations = p.getInt("itr", 10000);
-        System.out.println("6666 iterations: " + iterations + " size: " + size);
-      }
-
-      @Override
-      public void run(SourceContext<CollectiveData> sourceContext) throws Exception {
-        while (count < iterations) {
-          CollectiveData i = new CollectiveData(size);
-          sourceContext.collect(i);
-          count++;
-        }
-      }
-
-      @Override
-      public void cancel() {
-      }
-    });
-
-    stringStream.map(new RichMapFunction<CollectiveData, Tuple2<Integer, CollectiveData>>() {
-      @Override
-      public Tuple2<Integer, CollectiveData> map(CollectiveData s) throws Exception {
-        return new Tuple2<Integer, CollectiveData>(0, s);
-      }
-    }).keyBy(0).countWindow(1).aggregate(new AggregateFunction<Tuple2<Integer,CollectiveData>, List<CollectiveData>, List<CollectiveData>>() {
-      @Override
-      public List<CollectiveData> createAccumulator() {
-        return new ArrayList<>();
-      }
-
-      @Override
-      public List<CollectiveData> add(Tuple2<Integer, CollectiveData> integerCollectiveDataTuple2, List<CollectiveData> collectiveData) {
-        collectiveData.add(integerCollectiveDataTuple2.f1);
-        return collectiveData;
-      }
-
-
-      @Override
-      public List<CollectiveData> getResult(List<CollectiveData> collectiveData) {
-        return collectiveData;
-      }
-
-      @Override
-      public List<CollectiveData> merge(List<CollectiveData> collectiveData, List<CollectiveData> acc1) {
-        List<CollectiveData> d = new ArrayList<>();
-        d.addAll(collectiveData);
-        d.addAll(acc1);
-        return d;
-      }
-    }).addSink(new RichSinkFunction<List<CollectiveData>>() {
-      @Override
-      public void invoke(List<CollectiveData> collectiveData) throws Exception {
-        if (count == 0) {
-          start = System.nanoTime();
-        }
-        count++;
-        if (count >= iterations) {
-          System.out.println("Final: " + count + " " + (System.nanoTime() - start) / 1000000 + " " + (collectiveData.size()));
-        }
-      }
-
-      long start;
-      int count = 0;
-      int iterations;
-
-      @Override
-      public void open(Configuration parameters) throws Exception {
-        super.open(parameters);
-        ParameterTool p = (ParameterTool)
-            getRuntimeContext().getExecutionConfig().getGlobalJobParameters();
-        iterations = p.getInt("itr", 10000);
-        System.out.println("7777 iterations: " + iterations);
-      }
-    });
-
-  }
-
-  private static CollectiveData add(CollectiveData i, CollectiveData j) {
-    int[] r= new int[i.getList().length];
-    for (int k = 0; k < i.getList().length; k++) {
-      r[k] = ((i.getList()[k] + j.getList()[k]));
+    public StreamingGather(int size, int iterations, StreamExecutionEnvironment env, String outFile) {
+        this.size = size;
+        this.iterations = iterations;
+        this.env = env;
+        this.outFile = outFile;
     }
-    return new CollectiveData(r);
-  }
+
+    public void execute() {
+        DataStream<CollectiveData> stringStream = env.addSource(new RichParallelSourceFunction<CollectiveData>() {
+            int i = 1;
+            int count = 0;
+            int size = 0;
+            int iterations = 10000;
+
+            @Override
+            public void open(Configuration parameters) throws Exception {
+                super.open(parameters);
+                ParameterTool p = (ParameterTool)
+                        getRuntimeContext().getExecutionConfig().getGlobalJobParameters();
+                size = p.getInt("size", 128000);
+                iterations = p.getInt("itr", 10000);
+                //System.out.println("6666 iterations: " + iterations + " size: " + size);
+            }
+
+            @Override
+            public void run(SourceContext<CollectiveData> sourceContext) throws Exception {
+                while (count < iterations) {
+                    CollectiveData i = new CollectiveData(size, count);
+                    sourceContext.collect(i);
+                    count++;
+                }
+            }
+
+            @Override
+            public void cancel() {
+            }
+        });
+
+        stringStream.map(new RichMapFunction<CollectiveData, Tuple2<Integer, CollectiveData>>() {
+            @Override
+            public Tuple2<Integer, CollectiveData> map(CollectiveData s) throws Exception {
+                return new Tuple2<Integer, CollectiveData>(0, s);
+            }
+        }).keyBy(0).countWindow(1).aggregate(new AggregateFunction<Tuple2<Integer, CollectiveData>, List<CollectiveData>, List<CollectiveData>>() {
+            @Override
+            public List<CollectiveData> createAccumulator() {
+                return new ArrayList<>();
+            }
+
+            @Override
+            public List<CollectiveData> add(Tuple2<Integer, CollectiveData> integerCollectiveDataTuple2, List<CollectiveData> collectiveData) {
+                collectiveData.add(integerCollectiveDataTuple2.f1);
+                return collectiveData;
+            }
+
+
+            @Override
+            public List<CollectiveData> getResult(List<CollectiveData> collectiveData) {
+                return collectiveData;
+            }
+
+            @Override
+            public List<CollectiveData> merge(List<CollectiveData> collectiveData, List<CollectiveData> acc1) {
+                List<CollectiveData> d = new ArrayList<>();
+                d.addAll(collectiveData);
+                d.addAll(acc1);
+                return d;
+            }
+        }).addSink(new RichSinkFunction<List<CollectiveData>>() {
+            @Override
+            public void invoke(List<CollectiveData> collectiveData) throws Exception {
+                if (count == 0) {
+                    start = System.nanoTime();
+                }
+                count++;
+
+
+                if (count >= iterations) {
+                    CollectiveData c = collectiveData.get(0);
+                    long timeNow = System.nanoTime();
+                    String hostInfo = GetInfo.hostInfo();
+
+//                        System.out.println("sink,"
+//                                + count + ","
+//                                + c.getIteration() + ","
+//                                + c.getIterationString() + ","
+//                                + c.getMessageTime() + ","
+//                                + timeNow
+//                                + "," + (timeNow - c.getMessageTime()) / 1000000.0 + ","
+//                                + hostInfo + ","
+//                                + c.getMeta() + ","
+//                                + c.getList().length);
+                    System.out.println(count + ", " + c.getIteration() + ", " + timeNow + "," + c.getList().length);
+
+
+                    //System.out.println("Final: " + count + " " + (System.nanoTime() - start) / 1000000 + " " + (integerStringTuple2.f1));
+                }
+            }
+
+            long start;
+            int count = 0;
+            int iterations;
+
+            @Override
+            public void open(Configuration parameters) throws Exception {
+                super.open(parameters);
+                ParameterTool p = (ParameterTool)
+                        getRuntimeContext().getExecutionConfig().getGlobalJobParameters();
+                iterations = p.getInt("itr", 10000);
+                //System.out.println("7777 iterations: " + iterations);
+            }
+        });
+
+    }
+
+    private static CollectiveData add(CollectiveData i, CollectiveData j) {
+        int[] r = new int[i.getList().length];
+        for (int k = 0; k < i.getList().length; k++) {
+            r[k] = ((i.getList()[k] + j.getList()[k]));
+        }
+        return new CollectiveData(r);
+    }
 }
--- flink/performance/src/main/java/edu/iu/dsc/tws/flinkapps/stream/StreamingReduce.java
+++ flink/performance/src/main/java/edu/iu/dsc/tws/flinkapps/stream/StreamingReduce.java
@@ -1,6 +1,7 @@
 package edu.iu.dsc.tws.flinkapps.stream;
 
 import edu.iu.dsc.tws.flinkapps.data.CollectiveData;
+import edu.iu.dsc.tws.flinkapps.util.GetInfo;
 import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.common.functions.RichMapFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
@@ -15,93 +16,112 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StreamingReduce {
-  int size;
-  int iterations;
-  StreamExecutionEnvironment env;
-  String outFile;
+    int size;
+    int iterations;
+    StreamExecutionEnvironment env;
+    String outFile;
 
-  public StreamingReduce(int size, int iterations, StreamExecutionEnvironment env, String outFile) {
-    this.size = size;
-    this.iterations = iterations;
-    this.env = env;
-    this.outFile = outFile;
-  }
-
-  public void execute() {
-    DataStream<CollectiveData> stringStream = env.addSource(new RichParallelSourceFunction<CollectiveData>() {
-      int i = 1;
-      int count = 0;
-      int size = 0;
-      int iterations = 10000;
-
-      @Override
-      public void open(Configuration parameters) throws Exception {
-        super.open(parameters);
-        ParameterTool p = (ParameterTool)
-            getRuntimeContext().getExecutionConfig().getGlobalJobParameters();
-        size = p.getInt("size", 128000);
-        iterations = p.getInt("itr", 10000);
-        System.out.println("6666 iterations: " + iterations + " size: " + size);
-      }
-
-      @Override
-      public void run(SourceContext<CollectiveData> sourceContext) throws Exception {
-        while (count < iterations) {
-          CollectiveData i = new CollectiveData(size);
-          sourceContext.collect(i);
-          count++;
-        }
-      }
-
-      @Override
-      public void cancel() {
-      }
-    });
-
-    stringStream.map(new RichMapFunction<CollectiveData, Tuple2<Integer, CollectiveData>>() {
-      @Override
-      public Tuple2<Integer, CollectiveData> map(CollectiveData s) throws Exception {
-        return new Tuple2<Integer, CollectiveData>(0, s);
-      }
-    }).keyBy(0).countWindow(1).reduce(new ReduceFunction<Tuple2<Integer, CollectiveData>>() {
-      @Override
-      public Tuple2<Integer, CollectiveData> reduce(Tuple2<Integer, CollectiveData> c1,
-                                                    Tuple2<Integer, CollectiveData> c2) throws Exception {
-        return new Tuple2<Integer, CollectiveData>(0, add(c1.f1, c2.f1));
-      }
-    }).addSink(new RichSinkFunction<Tuple2<Integer,CollectiveData>>() {
-      long start;
-      int count = 0;
-      int iterations;
-
-      @Override
-      public void open(Configuration parameters) throws Exception {
-        super.open(parameters);
-        ParameterTool p = (ParameterTool)
-            getRuntimeContext().getExecutionConfig().getGlobalJobParameters();
-        iterations = p.getInt("itr", 10000);
-        System.out.println("7777 iterations: " + iterations);
-      }
-
-      @Override
-      public void invoke(Tuple2<Integer, CollectiveData> integerStringTuple2) throws Exception {
-        if (count == 0) {
-          start = System.nanoTime();
-        }
-        count++;
-        if (count >= iterations) {
-          System.out.println("Final: " + count + " " + (System.nanoTime() - start) / 1000000 + " " + (integerStringTuple2.f1));
-        }
-      }
-    });
-
-  }
-
-  private static CollectiveData add(CollectiveData i, CollectiveData j) {
-    int[] r= new int[i.getList().length];
-    for (int k = 0; k < i.getList().length; k++) {
-      r[k] = ((i.getList()[k] + j.getList()[k]));
+    public StreamingReduce(int size, int iterations, StreamExecutionEnvironment env, String outFile) {
+        this.size = size;
+        this.iterations = iterations;
+        this.env = env;
+        this.outFile = outFile;
     }
-    return new CollectiveData(r);
-  }
+
+    public void execute() {
+        DataStream<CollectiveData> stringStream = env.addSource(new RichParallelSourceFunction<CollectiveData>() {
+            int i = 1;
+            int count = 0;
+            int size = 0;
+            int iterations = 10000;
+
+            @Override
+            public void open(Configuration parameters) throws Exception {
+                super.open(parameters);
+                ParameterTool p = (ParameterTool)
+                        getRuntimeContext().getExecutionConfig().getGlobalJobParameters();
+                size = p.getInt("size", 128000);
+                iterations = p.getInt("itr", 10000);
+                //System.out.println("6666 iterations: " + iterations + " size: " + size);
+            }
+
+            @Override
+            public void run(SourceContext<CollectiveData> sourceContext) throws Exception {
+                while (count < iterations) {
+                    CollectiveData i = new CollectiveData(size, count);
+                    sourceContext.collect(i);
+                    //System.out.println(i.getSummary());
+                    count++;
+                }
+            }
+
+            @Override
+            public void cancel() {
+            }
+        });
+
+        stringStream.map(new RichMapFunction<CollectiveData, Tuple2<Integer, CollectiveData>>() {
+            @Override
+            public Tuple2<Integer, CollectiveData> map(CollectiveData s) throws Exception {
+                return new Tuple2<Integer, CollectiveData>(0, s);
+            }
+        }).keyBy(0).countWindow(1).reduce(new ReduceFunction<Tuple2<Integer, CollectiveData>>() {
+            @Override
+            public Tuple2<Integer, CollectiveData> reduce(Tuple2<Integer, CollectiveData> c1,
+                                                          Tuple2<Integer, CollectiveData> c2) throws Exception {
+                return new Tuple2<Integer, CollectiveData>(0, add(c1.f1, c2.f1));
+            }
+        }).addSink(new RichSinkFunction<Tuple2<Integer, CollectiveData>>() {
+            long start;
+            int count = 0;
+            int iterations;
+
+            @Override
+            public void open(Configuration parameters) throws Exception {
+                super.open(parameters);
+                ParameterTool p = (ParameterTool)
+                        getRuntimeContext().getExecutionConfig().getGlobalJobParameters();
+                iterations = p.getInt("itr", 10000);
+                //System.out.println("7777 iterations: " + iterations);
+            }
+
+            @Override
+            public void invoke(Tuple2<Integer, CollectiveData> integerStringTuple2) throws Exception {
+                if (count == 0) {
+                    start = System.nanoTime();
+                }
+                count++;
+                CollectiveData c = integerStringTuple2.f1;
+                if (count >= iterations && c != null) {
+
+                    long timeNow = System.nanoTime();
+                    String hostInfo = GetInfo.hostInfo();
+                    if (c.getMeta().equals(hostInfo)) {
+//                        System.out.println("sink,"
+//                                + count + ","
+//                                + c.getIteration() + ","
+//                                + c.getIterationString() + ","
+//                                + c.getMessageTime() + ","
+//                                + timeNow
+//                                + "," + (timeNow - c.getMessageTime()) / 1000000.0 + ","
+//                                + hostInfo + ","
+//                                + c.getMeta() + ","
+//                                + c.getList().length);
+                        System.out.println(count + ", " + c.getIteration() + ", " + timeNow + "," + c.getList().length);
+                    }
+
+                    //System.out.println("Final: " + count + " " + (System.nanoTime() - start) / 1000000 + " " + (integerStringTuple2.f1));
+                }
+            }
+        });
+
+    }
+
+    private static CollectiveData add(CollectiveData i, CollectiveData j) {
+        int[] r = new int[i.getList().length];
+        for (int k = 0; k < i.getList().length; k++) {
+            r[k] = ((i.getList()[k] + j.getList()[k]));
+        }
+        return new CollectiveData(r, i.getIteration() + ":" + j.getIteration());
+    }
 }
